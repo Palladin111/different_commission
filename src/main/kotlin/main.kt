c67eb30250a6757f2026f75a84e3704e5e183234
naum mazor <maznam1@mail.ru>
2023-10-15 21:01:28 +0300

    Changed main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,34 @@
 fun main() {
-    val moneyTransfer = 12500f
+    val moneyTransfer = 14100f
     val previousСash = 85000f
-    val typeCard = "Visa"
+    val typeCard = "VK Pay"
+    val transfer: Float
+    val monthLimit = 12000f
 
-    val transfer = moneyTransfer + getMoneyComission(typeCard, previousСash, moneyTransfer)
+    when (typeCard) {
 
-    println(transfer)
+        "Mastercard", "Maestro", "Visa", "Мир" -> if (moneyTransfer <= 150000f && monthLimit <= 600000f) {
+            transfer = moneyTransfer + getMoneyComission(typeCard, previousСash, moneyTransfer)
+            println(transfer)
+        } else {
+            println("Превышены суточный и/или месячный лимиты на перевод денег.")
+        }
+
+        "VK Pay" -> if (moneyTransfer <= 15000f && monthLimit <= 40000) {
+            transfer = moneyTransfer + getMoneyComission(typeCard, previousСash, moneyTransfer)
+            println(transfer)
+        } else {
+            println("Превышены суточный и/или месячный лимиты на перевод денег.")
+        }
+
+        else -> println("Вы ошиблись при вводе платежной системы")
+    }
 }
 
 fun getMoneyComission(typeCard: String = "Vk pay", previousСash: Float = 0f, moneyTransfer: Float): Float {
 
     val comission = when (typeCard) {
-        "Mastercard" -> if (previousСash <= 75000) 0f else moneyTransfer * 0.006f + 20
+        "Mastercard" -> if (previousСash + moneyTransfer <= 75000) 0f else moneyTransfer * 0.006f + 20
         "Maestro" -> if (previousСash <= 75000) 0f else moneyTransfer * 0.006f + 20
         "Visa" -> if (moneyTransfer > 35) moneyTransfer * 0.0075f else 35f
         "Мир" -> if (moneyTransfer > 35) moneyTransfer * 0.0075f else 35f
