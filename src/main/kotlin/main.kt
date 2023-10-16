781636ab72701724bb5e5a47baff2ec0a6e4e461
naum mazor <maznam1@mail.ru>
2023-10-16 21:30:17 +0300

    Changed main.kt, fixed limits

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,38 +1,40 @@
 fun main() {
-    val moneyTransfer = 14100f
-    val previousСash = 85000f
-    val typeCard = "VK Pay"
+
+    val moneyTransfer = 11100f
+    val previousСash = 78000f
+    val typeCard = "Mastercard"
     val transfer: Float
-    val monthLimit = 12000f
+    val comission = getMoneyComission(typeCard, previousСash, moneyTransfer)
 
-    when (typeCard) {
-
-        "Mastercard", "Maestro", "Visa", "Мир" -> if (moneyTransfer <= 150000f && monthLimit <= 600000f) {
-            transfer = moneyTransfer + getMoneyComission(typeCard, previousСash, moneyTransfer)
-            println(transfer)
-        } else {
-            println("Превышены суточный и/или месячный лимиты на перевод денег.")
-        }
-
-        "VK Pay" -> if (moneyTransfer <= 15000f && monthLimit <= 40000) {
-            transfer = moneyTransfer + getMoneyComission(typeCard, previousСash, moneyTransfer)
-            println(transfer)
-        } else {
-            println("Превышены суточный и/или месячный лимиты на перевод денег.")
-        }
-
-        else -> println("Вы ошиблись при вводе платежной системы")
+    if (comission >= 0) {
+        transfer = moneyTransfer + comission
+        println(transfer)
+    } else {
+        println("Перевод не возможен - превышены суточные и/или месячные лимиты")
     }
 }
 
 fun getMoneyComission(typeCard: String = "Vk pay", previousСash: Float = 0f, moneyTransfer: Float): Float {
 
     val comission = when (typeCard) {
-        "Mastercard" -> if (previousСash + moneyTransfer <= 75000) 0f else moneyTransfer * 0.006f + 20
-        "Maestro" -> if (previousСash <= 75000) 0f else moneyTransfer * 0.006f + 20
-        "Visa" -> if (moneyTransfer > 35) moneyTransfer * 0.0075f else 35f
-        "Мир" -> if (moneyTransfer > 35) moneyTransfer * 0.0075f else 35f
-        else -> 0f
+
+        "Mastercard", "Maestro" -> if (moneyTransfer <= 150000f && previousСash <= 600000f) {
+            if (previousСash + moneyTransfer <= 75000) 0f else moneyTransfer * 0.006f + 20
+        } else {
+            -1f
+        }
+
+        "Visa", "Мир" -> if (moneyTransfer <= 150000f && previousСash <= 600000f) {
+            if (previousСash + moneyTransfer <= 75000) 0f else moneyTransfer * 0.006f + 20
+        } else {
+            -1f
+        }
+
+        else -> if (moneyTransfer <= 15000f && previousСash <= 40000f) {
+            0f
+        } else {
+            -1f
+        }
     }
 
     return comission
